0413e726c59353ff86d7088547378a28e37c7ef3
nyxjemk <32337316+nyxjemk@users.noreply.github.com>
2017-10-24 10:30:10 +0200

    Update make_wordvectors.sh

--- make_wordvectors.sh
+++ make_wordvectors.sh
@@ -2,7 +2,7 @@
 
 #### Set your hyper-parameters here ####
 ############## START ###################
-lcode="xx" # ISO 639-1 code of target language. See `lcodes.txt`.
+lcode="sv" # ISO 639-1 code of target language. See `lcodes.txt`.
 max_corpus_size=1000000000 # the maximum size of the corpus. Feel free to adjust it according to your computing power.
 vector_size=150 # the size of a word vector
 window_size=5 # the maximum distance between the current and predicted word within a sentence.
