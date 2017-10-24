0d0a8465e6a3a53523ab4edac1a14be9d7f8544a
nyxjemk <32337316+nyxjemk@users.noreply.github.com>
2017-10-24 15:26:40 +0200

    Update make_wordvectors.sh

--- make_wordvectors.sh
+++ make_wordvectors.sh
@@ -3,10 +3,10 @@
 #### Set your hyper-parameters here ####
 ############## START ###################
 lcode="sv" # ISO 639-1 code of target language. See `lcodes.txt`.
-max_corpus_size=4000000000 # the maximum size of the corpus. Feel free to adjust it according to your computing power.
-vector_size=150 # the size of a word vector
+max_corpus_size=3000000000 # the maximum size of the corpus. Feel free to adjust it according to your computing power.
+vector_size=300 # the size of a word vector
 window_size=5 # the maximum distance between the current and predicted word within a sentence.
-vocab_size=500000 # the maximum vocabulary size
+vocab_size=750000 # the maximum vocabulary size
 num_negative=5 # the int for negative specifies how many “noise words” should be drawn
 ############## END #####################
 
